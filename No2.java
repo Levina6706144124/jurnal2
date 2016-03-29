002d125b95e95827709b9bac9c22d9c28550d086
Levina Yunita <levinayunita@gmail.com>
2016-03-29 10:23:38 +0700

    Added files via upload

---
+++ No2.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no2;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class No2 {
+
+    /**
+     * @param args the command line arguments
+     */
+      public static long fib(int n) {
+        if (n <= 1) return n;
+        else return fib(n-1) + fib(n-2);
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+ 
+
+    /**
+     * @param args the command line arguments
+     */
+   
+     
+   
+  int l = 1;
+int tingkat = 10;
+int n = 12/2;
+for(int i=1;i<=n;i++){
+for(int k=n;k>i;k--){
+System.out.print(" ");
+}
+for(int j=1;j<=l;j++){
+System.out.print("+");
+}
+l+=2;
+ 
+System.out.println("");
+}
+l -= 4;
+ 
+for(int i=(n-1);i>=1;i--){
+for(int k=i;k<=(n-1);k++){
+System.out.print(" ");
+}
+for(int j=l;j>=1;j--){
+System.out.print("+");
+}
+l-=2;
+ 
+System.out.println("");
+          
+    }
+    }
+}
+    
+    
+
